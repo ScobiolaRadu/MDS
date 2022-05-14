b1ceb069b002be698987d3ab998f96c35378897b
ScobiolaRadu <80578568+ScobiolaRadu@users.noreply.github.com>
2022-05-14 03:48:53 +0300

    Etapa 1

---
+++ Mds/Assets/MenuController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuController : MonoBehaviour
+{
+    [Header("Volume Setting")]
+    [SerializeField] private TextMeshProUGUI volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+    [Header("Levels To Load")]
+    [SerializeField] public string _newGameLevel;
+    private string levelToLoad;
+    [SerializeField] private GameObject noSavedGameDialog = null;
+
+    public void NewGameDialogYes()
+    {
+        SceneManager.LoadScene(_newGameLevel);
+    }
+
+    public void LoadGameDialogYes()
+    {
+        if(PlayerPrefs.HasKey("SavedLevel"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else
+        {
+            noSavedGameDialog.SetActive(true);
+        }
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
+    }
+}
---
+++ Mds/Assets/animationStateController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isRunningForwardHash;
+    int isStrafingLeftHash;
+    int isStrafingRightHash;
+    int isRunningBackwardHash;
+    int isJumpingHash;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isRunningForwardHash = Animator.StringToHash("isRunningForward");
+        isStrafingLeftHash = Animator.StringToHash("isStrafingLeft");
+        isStrafingRightHash = Animator.StringToHash("isStrafingRight");
+        isRunningBackwardHash = Animator.StringToHash("isRunningBackward");
+        isJumpingHash = Animator.StringToHash("isJumping");
+ 
+    }
+
+    void Update()
+    {
+        bool isRunningForward = animator.GetBool(isRunningForwardHash);
+        bool forwardPressed = Input.GetKey("w");
+
+        if (!isRunningForward && forwardPressed)
+            animator.SetBool(isRunningForwardHash, true);
+        if(isRunningForward && !forwardPressed)
+            animator.SetBool(isRunningForwardHash, false);
+
+
+        bool isRunningBackward = animator.GetBool(isRunningBackwardHash);
+        bool backwardPressed = Input.GetKey("s");
+
+        if (!isRunningBackward && backwardPressed)
+            animator.SetBool(isRunningBackwardHash, true);
+        if (isRunningBackward && !backwardPressed)
+            animator.SetBool(isRunningBackwardHash, false);
+
+        bool isStrafingLeft = animator.GetBool(isStrafingLeftHash);
+        bool leftPressed = Input.GetKey("a");
+
+        if (!isStrafingLeft && leftPressed)
+            animator.SetBool(isStrafingLeftHash, true);
+        if (isStrafingLeft && !leftPressed)
+            animator.SetBool(isStrafingLeftHash, false);
+
+        bool isStrafingRight = animator.GetBool(isStrafingRightHash);
+        bool rightPressed = Input.GetKey("d");
+
+        if (!isStrafingRight && rightPressed)
+            animator.SetBool(isStrafingRightHash, true);
+        if (isStrafingRight && !rightPressed)
+            animator.SetBool(isStrafingRightHash, false);
+
+        bool isJumping = animator.GetBool(isJumpingHash);
+        bool jumpPressed = Input.GetKey("space");
+
+        if (!isJumping && jumpPressed)
+            animator.SetBool(isJumpingHash, true);
+        if (isJumping && !jumpPressed)
+            animator.SetBool(isJumpingHash, false);
+
+    }
+}
